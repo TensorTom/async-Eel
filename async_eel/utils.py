27012e02d7d599d7f8f3f4c84e64ea6214305360
namuyan <thhjuu@yahoo.co.jp>
2019-06-19 00:16:20 +0900

    add: method run_in_another_thread

--- async_eel/utils.py
+++ async_eel/utils.py
@@ -1,6 +1,7 @@
 from aiohttp.web import BaseRequest
 from aiohttp import web
 from logging import getLogger
+from threading import Thread
 import asyncio
 
 
@@ -19,6 +20,22 @@ async def websocket_protocol_check(request: BaseRequest):
     return ws
 
 
+async def run_in_another_thread(fnc, *args, **kwargs):
+    """run blocking fnc in another thread"""
+    future = asyncio.Future()
+
+    def waiter():
+        try:
+            future.set_result(fnc(*args, **kwargs))
+        except Exception as e:
+            future.set_exception(e)
+
+    Thread(target=waiter).start()
+    await future
+    return future.result()
+
+
 __all__ = [
     "websocket_protocol_check",
+    "run_in_another_thread",
 ]
